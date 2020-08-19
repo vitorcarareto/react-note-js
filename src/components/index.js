def2ebd0a409038419d831bc7abd1677d43b9060
vitorcarareto <vitor.carareto@gmail.com>
2020-08-19 07:41:17 -0300

    65 Modularização do header

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,15 @@ import NewNote from "./NewNote/NewNote";
 import Note from "./Note/Note";
 import NoteList from "./NoteList/NoteList";
 import PageLayout from "./PageLayout/PageLayout";
+import Header from "./Header/Header";
 
-export { Error, NewNote, Note, NoteList, PageLayout, AppBar, NavigationDrawer };
+export {
+    Error,
+    NewNote,
+    Note,
+    NoteList,
+    PageLayout,
+    AppBar,
+    NavigationDrawer,
+    Header,
+};
