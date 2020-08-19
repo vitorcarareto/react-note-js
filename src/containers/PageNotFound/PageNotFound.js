a2031bd0e6c33de5632f86ff19a5a24566f98a16
vitorcarareto <vitor.carareto@gmail.com>
2020-08-19 19:04:28 -0300

    68 - Imagem página não encontrada

--- src/containers/PageNotFound/PageNotFound.js
+++ src/containers/PageNotFound/PageNotFound.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
+import NotFound from "../../images/notfound.gif";
 import { Header, ButtonLink, Center } from "../../components";
 
 const PageNotFound = () => (
     <Center>
         <Header>Ops!</Header>
+        <div>
+            <img src={NotFound} alt="Página não encontrada." width="200" />
+        </div>
         <p>Página não encontrada.</p>
         <ButtonLink to="/">Voltar para o início</ButtonLink>
     </Center>
