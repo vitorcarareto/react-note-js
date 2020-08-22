1c3f686a7f0f674288d714a8fd50272b34b33382
vitorcarareto <vitor.carareto@gmail.com>
2020-08-22 18:03:30 -0300

    77 - Refinando o App

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,25 @@
-import AppBar from "./AppBar/AppBar";
-import NavigationDrawer from "./NavigationDrawer/NavigationDrawer";
 import Error from "./Error/Error";
 import NewNote from "./NewNote/NewNote";
 import Note from "./Note/Note";
 import NoteList from "./NoteList/NoteList";
-import PageLayout from "./PageLayout/PageLayout";
 import Header from "./Header/Header";
 import Button from "./Button/Button";
 import Center from "./Center/Center";
 import ButtonLink from "./Button/ButtonLink";
+import AppBar from "./AppBar/AppBar";
+import NavigationDrawer from "./NavigationDrawer/NavigationDrawer";
+import Container from "./Container/Container";
 
 export {
     Error,
     NewNote,
     Note,
     NoteList,
-    PageLayout,
-    AppBar,
-    NavigationDrawer,
     Header,
     Button,
     ButtonLink,
     Center,
+    AppBar,
+    NavigationDrawer,
+    Container,
 };
