b065da3bd29956deaade6f3395adf74a4e6ee584
vitorcarareto <vitor.carareto@gmail.com>
2020-08-22 12:20:49 -0300

    74 - Higher-Order Components

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,49 +1,46 @@
 import React from "react";
 
-import SettingsContext from "../../containers/Settings/SettingsContext";
+import withSettings from "../../containers/Settings/withSettings";
 
 import "./app-bar.scss";
 
-const AppBar = ({ isLoading, saveHasError, onSaveRetry, onOpenMenu }) => {
+const AppBar = ({
+    isLoading,
+    saveHasError,
+    onSaveRetry,
+    onOpenMenu,
+    theme,
+}) => {
     return (
-        <SettingsContext.Consumer>
-            {({ theme }) => (
-                <div
-                    className="app-bar"
-                    style={{ backgroundColor: theme.colorPrimary }}
-                >
-                    <div className="app-bar__container">
-                        <button
-                            className="app-bar__action"
-                            onClick={onOpenMenu}
-                        >
-                            <i className="material-icons material-icons-round">
-                                menu
-                            </i>
-                        </button>
-                        <span className="app-bar__brand">Note.js</span>
-                        {isLoading && (
-                            <button className="app-bar__action app-bar__action--rotation">
-                                <i className="material-icons material-icons-round">
-                                    refresh
-                                </i>
-                            </button>
-                        )}
-                        {saveHasError && (
-                            <button
-                                className="app-bar__action app-bar__action--danger"
-                                onClick={onSaveRetry}
-                            >
-                                <i className="material-icons material-icons-round">
-                                    cloud_off
-                                </i>
-                            </button>
-                        )}
-                    </div>
-                </div>
-            )}
-        </SettingsContext.Consumer>
+        <div
+            className="app-bar"
+            style={{ backgroundColor: theme.colorPrimary }}
+        >
+            <div className="app-bar__container">
+                <button className="app-bar__action" onClick={onOpenMenu}>
+                    <i className="material-icons material-icons-round">menu</i>
+                </button>
+                <span className="app-bar__brand">Note.js</span>
+                {isLoading && (
+                    <button className="app-bar__action app-bar__action--rotation">
+                        <i className="material-icons material-icons-round">
+                            refresh
+                        </i>
+                    </button>
+                )}
+                {saveHasError && (
+                    <button
+                        className="app-bar__action app-bar__action--danger"
+                        onClick={onSaveRetry}
+                    >
+                        <i className="material-icons material-icons-round">
+                            cloud_off
+                        </i>
+                    </button>
+                )}
+            </div>
+        </div>
     );
 };
 
-export default AppBar;
+export default withSettings(AppBar);
