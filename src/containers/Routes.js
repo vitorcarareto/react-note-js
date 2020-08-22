4f8c5e5ca3f74668fdd9711429e83f161350ab46
vitorcarareto <vitor.carareto@gmail.com>
2020-08-22 08:07:54 -0300

    Lista de notas (preview)

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -7,7 +7,7 @@ import SettingsPage from "./Settings/SettingsPage";
 import PageNotFound from "./PageNotFound/PageNotFound";
 
 export const menu = [
-    { icon: "notes", label: "Listas de Notas", path: "/list" },
+    { icon: "notes", label: "Listas de Notas (em breve)", path: "/list" },
     { icon: "sticky_note_2", label: "Notas", path: "/" },
     { icon: "settings", label: "Configurações", path: "/settings" },
     { icon: "info", label: "Sobre", path: "/about" },
