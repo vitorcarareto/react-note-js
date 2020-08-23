2a2b03ee50ff6063b69660a4d2912cb0c6e646c5
vitorcarareto <vitor.carareto@gmail.com>
2020-08-23 08:10:32 -0300

    77 - Refinando o App

--- src/containers/Settings/withSettings.js
+++ src/containers/Settings/withSettings.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import SettingsContext from "./SettingsContext";
 
-const withSettings = Component => props => (
+const withSettings = (Component) => (props) => (
     <SettingsContext.Consumer>
-        {context => <Component {...props} {...context} />}
+        {(context) => <Component {...props} {...context} />}
     </SettingsContext.Consumer>
 );
 
